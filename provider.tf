76094fb2d617bb7a6471d631e9480588539b58b5
root <root@MKT-MCP.localdomain>
2026-03-13 16:07:51 -0300

    feat: pre commit

--- provider.tf
+++ provider.tf
@@ -5,8 +5,13 @@ terraform {
       version = "6.36.0"
     }
   }
+  backend "s3" {
+    bucket = "awscourse-s3-288757602165-us-east-1-an"
+    key    = "eks-lab/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
