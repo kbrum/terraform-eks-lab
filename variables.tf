e17bf688f77ad55121e63b3950d951286b5646cf
Kbrum <kaykyazevedo97@gmail.com>
2026-03-25 23:27:28 -0300

    docs: add docs file

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "cidr_block" {
   type        = string
-  description = "Networkin cidr block to be used for the VPC"
+  description = "Networking cidr block to be used for the VPC"
 }
 
 variable "project_name" {
